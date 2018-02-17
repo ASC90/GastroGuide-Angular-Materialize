02e0d8abb5d3d4c66d6af45c4b56442e12cacbfe
Unknown <arnausantoscabre@gmail.com>
2018-02-17 22:40:02 +0100

    Buscador_2

--- GastroGuide/src/app/buscador/buscador.component.ts
+++ GastroGuide/src/app/buscador/buscador.component.ts
@@ -2,16 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 declare var $: any;
 
+// JSON de tipo de cocina
 const mockyTipoCocina = [
   "Española", "India", "Japonesa", "China",
   "Italiana", "Mexicana", "Tailandesa", "Coreana",
   "Mediterránea", "Americana", "Marroquí", "Escandinava"
 ];
-
+// JSON de ambientes
 const mockyAmbientes = [
   "Con amigos", "Romántico", "De fiesta", "Moderno",
   "Tradicional", "De negocios", "Para grupos", "Família",
   "Tranquilo","Ecológico"
+];
+// JSON ciudades
+const mockyCiudades = [
+  "Albacete", "Alicante", "Almería", "Ávila", "Badajoz", "Barcelona",
+  "Bilbao", "Burgos", "Cáceres", "Cádiz", "Castellón de la Plana",
+  "Ceuta", "Ciudad Real", "Córdoba", "Cuenca", "Gerona", "Granada", "Guadalajara",
+  "Huelva", "Huesca", "Jaén", "La Coruña", "Las Palmas de Gran Canaria", "León",
+  "Lérida", "Logroño", "Lugo", "Madrid", "Málaga", "Melilla", "Mérida", "Murcia",
+  "Orense", "Oviedo", "Palencia", "Palma", "Pamplona", "Pontevedra", "Salamanca",
+  "San Sebastián", "Santa Cruz de Tenerife", "Santander", "Santiago de Compostela",
+  "Segovia", "Sevilla", "Soria", "Tarragona", "Teruel", "Toledo", "Valencia", "Valladolid",
+  "Vitoria-Gasteiz", "Zamora", "Zaragoza"
 ];
 
 @Component({
@@ -19,15 +32,23 @@ const mockyAmbientes = [
   templateUrl: './buscador.component.html',
   styleUrls: ['./buscador.component.css']
 })
+
 export class BuscadorComponent implements OnInit {
   tipoDeCocina = mockyTipoCocina;
   ambientes = mockyAmbientes;
+  ciudades = mockyCiudades;
   optionsTipoCocina = [];
   optionsAmbientes = [];
   localidad = "";
-  fecha = new Date();
+  fecha;
   hora;
   comensales = "";
+  visivilidad = "invisible collection";
+  // Validaciones
+  validarTipoCocina = "invalid";
+  validarAmbiente = "invalid";
+  validarLocalidad = "invalid";
+  errorLocalidad = "El campo está vacío";
   constructor() { }
 
   ngOnInit() {
@@ -57,8 +78,65 @@ export class BuscadorComponent implements OnInit {
     });
   }
   getBuscador() {
+    this.getValidarTipoDeCocina();
     console.log("options", this.optionsTipoCocina, this.optionsAmbientes, this.localidad, this.fecha, this.hora, this.comensales);
-    if (this.optionsTipoCocina && this.optionsAmbientes && this.localidad && this.fecha && this.hora && this.comensales)
+    if (this.optionsTipoCocina && this.optionsAmbientes && this.localidad && this.fecha && this.hora && this.comensales) {
       alert("todo correcto");
+      let arr = [this.optionsTipoCocina, this.optionsAmbientes, this.localidad, this.fecha, this.hora, this.comensales];
+      localStorage.clear();
+      localStorage.setItem("busqueda", JSON.stringify(arr));
+    }
+  }
+  getValidarTipoDeCocina() {
+    if (this.optionsTipoCocina.length == 0 || !this.optionsTipoCocina) {
+      this.validarTipoCocina = "invalid";
+    }
+    else
+      this.validarTipoCocina = "valid";
+  }
+  getValidarAmbiente() {
+    if (this.optionsAmbientes.length == 0 || !this.optionsAmbientes) {
+      this.validarAmbiente = "invalid";
+    }
+    else
+      this.validarAmbiente = "valid";
+  }
+  getValidarLocalidad() {
+    let str = this.localidad;
+    this.ciudades = mockyCiudades;
+    this.ciudades = this.ciudades.filter(function (arr) {
+      if (arr.toUpperCase().includes(str.toUpperCase()))
+        return arr;
+    });
+    /*for (let i = 0; i < this.ciudades.length; i++) {
+      if (this.ciudades[i].toUpperCase().includes(this.localidad))
+        this.validarLocalidad = "valid";
+    }*/
+    if (mockyCiudades.includes(this.localidad)) {
+      this.validarLocalidad = "valid";
+    }
+    else if (this.localidad == "") {
+      this.errorLocalidad = "El campo está vacío";
+      this.validarLocalidad = "invalid";
+    }
+    else {
+      this.errorLocalidad = "Esta localidad no existe";
+      this.validarLocalidad = "invalid";
+    }
+      
+    console.log("ciudades", this.ciudades);
+    if (this.ciudades.length < 1 || this.localidad == "") {
+      this.visivilidad = "invisible collection";
+    }
+    else {
+      this.visivilidad = "visible collection";
+    }
+  }
+  selectCity(ciudad: string) {
+    console.log(ciudad);
+    this.localidad = ciudad;
+    this.visivilidad = "invisible collection";
+    if (mockyCiudades.includes(this.localidad))
+      this.validarLocalidad = "valid";
   }
 }
