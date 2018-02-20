aca1dfd1bda5f5f710064d243b01541fc5093f24
Daniil <dmihhedko@gmail.com>
2018-02-20 17:30:15 +0100

    form validacion finalizado

--- GastroGuide/src/app/registro-restaurante/registro-restaurante.component.ts
+++ GastroGuide/src/app/registro-restaurante/registro-restaurante.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { PatternValidator,FormGroup,FormControl,Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators, RadioControlValueAccessor } from '@angular/forms';
+import { matchOtherValidator } from '../1-match-other-validator'
+import { Router } from '@angular/router';
+
 
 
 
@@ -10,44 +13,45 @@ import { PatternValidator,FormGroup,FormControl,Validators} from '@angular/forms
   styleUrls: ['./registro-restaurante.component.css']
 })
 export class RegistroRestauranteComponent implements OnInit {
-  
-  // restaurante:string="";
-  // nombre:string="";
-  // apellidos:string="";
-  // genero:string="";
-  // email:string="";
-  // passwordText:string="";
-  // password2Text:string="";
-  // tel:number;
-  // cp:number;
-  // poblacion:string="";
-  // calle:string="";
-  // numer:string="";
 
-  // errornombre:string="Su nombre no puede contener números o caracteres especiales";
-  // errorapellidos:string="Su apellido no puede contener números o caracteres especiales";
-  // erroremail:string="Por favor introduzca su correo electrónico en formato correcto";
-  // errorcontrasena:string="Su contraseña debe contener por lo menos 8 caracteres y contener una letra mayúscula, una minúscula y un número";
-  // errortel:string="Por favor introduzca su teléfono en formato correcto";
-  // errorcp:string="Por favor introduzca su código postal en formato correcto";
- 
+  passRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})")
   form = new FormGroup({
-    restaurante: new FormControl("",Validators.required),
-    nombre: new FormControl("",[Validators.required,Validators.pattern("[\s a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]*")]),
-    apellidos: new FormControl("",[Validators.required,Validators.pattern("[\s a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]*")]),
-    genero: new FormControl("",Validators.required),
-    email: new FormControl("",[Validators.required,Validators.email]),
-    password: new FormControl("",[Validators.required]),
-    password2: new FormControl("",Validators.required),
-    tel: new FormControl("",[Validators.required,Validators.pattern("[0-9]{9}")]),
-    cp: new FormControl("",[Validators.required,Validators.pattern("[0-9]{5}")]),
-    poblacion: new FormControl("",Validators.required),
-    calle: new FormControl("",Validators.required),
-    numero: new FormControl("",Validators.required)
-  });
-  constructor() { }
+    restaurante: new FormControl("", Validators.required),
+    nombre: new FormControl("", [Validators.required, Validators.pattern("[\s a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]*")]),
+    apellidos: new FormControl("", [Validators.required, Validators.pattern("[\s a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]*")]),
+    genero: new FormControl("", Validators.required),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.pattern(this.passRegex)]),
+    password2: new FormControl("", [Validators.required, matchOtherValidator("password")]),
+    tel: new FormControl("", [Validators.required, Validators.pattern("[0-9]{9}")]),
+    cp: new FormControl("", [Validators.required, Validators.pattern("[0-9]{5}")]),
+    poblacion: new FormControl("", Validators.required),
+    calle: new FormControl("", Validators.required),
+    numero: new FormControl("", Validators.required),
+    condiciones: new FormControl(false, Validators.requiredTrue)
+  })
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    let gastroChef = {
+      restaurante: this.form.get("restaurante").value,
+      nombre: this.form.get("nombre").value,
+      apellidos: this.form.get("apellidos").value,
+      genero: this.form.get("genero").value,
+      email: this.form.get("email").value,
+      password: this.form.get("password").value,
+      tel: this.form.get("tel").value,
+      cp: this.form.get("cp").value,
+      poblacion: this.form.get("poblacion").value,
+      calle: this.form.get("calle").value,
+      numero: this.form.get("numero").value
+    }
+
+    this.router.navigateByUrl("/homeres");
   }
 
 }
